4aa3675d3a7a93bb712d0f9c38db74acad671cbb
noenemy <noenemy@gmail.com>
2021-10-15 15:03:00 +0900

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,88 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React from "react";
+import Navbar from './components/layout/Navbar';
+import HomeComponent from './components/home/HomeComponent';
+import LearningComponent from './components/learning/LearningComponent';
+import GameComponent from './components/game/GameComponent';
+import LeaderboardComponent from './components/leaderboard/LeaderBoardComponent';
+import DemoComponent from './components/demo/DemoComponent';
+import DebugComponent from './components/debug/DebugComponent';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <header>
+      <Router>
+          <Navbar title="1:1 Language Tutor using AWS AI/ML" />
+
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/learning">
+              <Learning />
+            </Route>
+            <Route path="/game">
+              <Game />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard />
+            </Route>
+            <Route path="/debug">
+              <Debug />
+            </Route>
+            <Route path="/demo">
+              <Demo />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+    </header>
+  );
+}
+
+function Home(){
+  return(
+    <HomeComponent />
+  )
+}
+
+function Learning(){
+  return(
+    <LearningComponent />
+  )
+}
+
+function Game(){
+  return(
+    <GameComponent />
+  )
+}
+
+function Leaderboard(){
+  return(
+      <LeaderboardComponent />
+  )
+}
+
+function Debug(){
+  return(
+    <DebugComponent />
+  )
+}
+
+function Demo(){
+
+  return(
+    <DemoComponent />
+  )
+}
+
+export default App;
